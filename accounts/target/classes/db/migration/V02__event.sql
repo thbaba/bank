a8d067faa10a6d907cc8d2808e13db8d1640942d
Can Deniz <dcb.denizcan@gmail.com>
2025-10-07 07:30:12 +0300

    .

--- accounts/target/classes/db/migration/V02__event.sql
+++
@@ -1,7 +0,0 @@
-create table events (
-    id serial primary key,
-    topic varchar(128) not null,
-    key int not null,
-    payload text not null,
-    status varchar(8) not null default 'PENDING'
-);
